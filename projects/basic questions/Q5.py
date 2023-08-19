852d6e8f36e95920aac8e3bf875ce39c6b87996c
sutanuadhikary <138210091+sutanuadhikary@users.noreply.github.com>
2023-08-19 23:58:31 +0530

    Update Q5.py

--- projects/basic questions/Q5.py
+++ projects/basic questions/Q5.py
@@ -1,3 +1,5 @@
+# Q5. What is the average family income of the students?
+
 import pandas as pd
 
 # Load the Excel file
@@ -9,3 +11,9 @@ student_details = df.loc[df['First Name'] == student_name]
 
 # Display the student details
 print(student_details)
+
+''' 
+Enter the name of the student: shubham
+    First Name Family Income
+639    shubham      0-2 Lakh
+'''
