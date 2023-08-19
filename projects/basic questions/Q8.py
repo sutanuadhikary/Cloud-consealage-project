cd9b04aa4195883fad2bfd7c3c36fa3e2b0ddf83
sutanuadhikary <138210091+sutanuadhikary@users.noreply.github.com>
2023-08-20 00:01:23 +0530

    Update Q8.py

--- projects/basic questions/Q8.py
+++ projects/basic questions/Q8.py
@@ -1,3 +1,5 @@
+# Q8. What is the average GPA for student from each city?
+
 import pandas as pd
 
 # Load the data from the Excel file
@@ -8,3 +10,18 @@ average_CGPA = data.groupby('City')['CGPA'].mean()
 
 # Print the result
 print('Average CCGPA for each city:', average_CGPA)
+
+'''
+Average CCGPA for each city: City
+Agartala     7.660714
+Agra         8.046429
+Ahemdabad    8.190385
+Ajmer        8.284314
+Akola        8.021429
+               ...
+Vidisha      7.738095
+Vijaywada    7.986364
+Wardha       8.328571
+konark       8.071429
+kullu        7.878571
+'''
