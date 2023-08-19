8d626db120981a98801f196eba82e11324cfc0f5
sutanuadhikary <138210091+sutanuadhikary@users.noreply.github.com>
2023-08-19 23:57:16 +0530

    Update Q4.py

--- projects/basic questions/Q4.py
+++ projects/basic questions/Q4.py
@@ -1,3 +1,5 @@
+#what is the distribution of student's experience with python programming?
+
 import pandas as pd
 
 # Read the data from the Excel file
@@ -8,3 +10,13 @@ x = data['Experience with python (Months)'].value_counts()
 
 # Print the value
 print(x)
+
+'''
+Experience with python (Months)
+5    1242
+3    1008
+8     800
+6     738
+7     640
+4     466
+'''
